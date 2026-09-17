42151a89d6b62c3232536ce55dd678db59e4920c
yerrayyamma6147-eng <yerrayyamma.6147@gmail.com>
2026-09-17 11:47:35 +0530

    Add subnet resource to network configuration

--- network.tf
+++ network.tf
@@ -4,3 +4,12 @@ resource "azurerm_virtual_network" "vnet1" {
   resource_group_name = azurerm_resource_group.RG.name
   address_space       = ["10.0.0.0/16"]
 }
+
+
+
+resource "azurerm_subnet" "example" {
+  name                 = "example-subnet"
+  resource_group_name  = azurerm_resource_group.example.name
+  virtual_network_name = azurerm_virtual_network.example.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
